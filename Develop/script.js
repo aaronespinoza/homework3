7f7383dd117ca364f4d7d92f05c1663b032174dd
aaronespinoza <aarone@berkeley.edu>
2021-11-15 20:00:03 -0800

    logic for character length prompt added

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 
-var optionsLength = [128<=index<=8];
+var optionsLength = 8;
 var optionsNum = [0,1,2,3,4,5,6,7,8,9];
 var optionsLow = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var optionsLow = ["A","B","C","D","E","F","G","H","I","J","L","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -18,13 +18,19 @@ var generateBtn = document.querySelector("#generate");
 //prompt for lowercase
 function characterLength (){
   
-  window.prompt("Enter a number between 8 and 128 for desired password length")
+  optionsLength = parseInt(prompt("Enter a number between 8 and 128 for desired password length"));
+
+  if  (isNaN(optionsLength)|| optionsLength < 8 || optionsLength> 128) {
+    alert("character length must be an integer 8-128 characters long");
+  }
+  
+
 };
 
 generateBtn.addEventListener("click", characterLength);
 
 
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 //generateBtn.textContent = "";
 //generateBtn.appendChild()
 
